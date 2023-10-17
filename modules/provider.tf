1060e0c5a097380d7c21e93c8160c9b1a5dc1d03
SamuelEBG <samuel.ebgonzales@gmail.com>
2023-10-17 10:39:52 +0200

    added default image and prefix variables

--- modules/provider.tf
+++ modules/provider.tf
@@ -5,9 +5,4 @@ terraform {
       version = "4.39.0"
     }
   }
-  backend "s3" {
-    bucket = "pgr301-2021-terraform-state"
-    key    = "sago004/apprunner-actions.state"
-    region = "eu-north-1"
-  }
 }
--- modules/variables.tf
+++ modules/variables.tf
@@ -1,7 +1,9 @@
 variable "prefix" {
   type = string
+  default = "user_name"
 }
 
 variable "image" {
   type = string
+  default = "image_name"
 }
